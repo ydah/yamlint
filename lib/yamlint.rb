8a95dbec5b96277db92999ae72668bb7a588f2fb
ydah <13041216+ydah@users.noreply.github.com>
2022-11-16 20:27:41 +0900

    Initial commit

---
+++ lib/yamlint.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "yamlint/version"
+
+module Yamlint
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/yamlint/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Yamlint
+  VERSION = "0.1.0"
+end
